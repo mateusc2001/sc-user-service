c9ad14b24413d9cc55959e49f6cd2a016ec82f8c
mateusc2001 <mateusc2001@gmail.com>
2022-10-30 12:53:55 -0300

    Adicionando senha no mapper

--- src/mapper/user-model.mapper.ts
+++ src/mapper/user-model.mapper.ts
@@ -1,19 +1,20 @@
-import { UserModel } from "src/model/user.model";
-import { UserDocument } from "src/schemas/user.schema";
+import { UserModel } from 'src/model/user.model';
+import { UserDocument } from 'src/schemas/user.schema';
 
 export class UserModelMapper {
+  public static mapToList(userModelList: UserDocument[]): UserModel[] {
+    return userModelList.map((item) => this.mapTo(item));
+  }
 
-    public static mapToList(userModelList: UserDocument[]): UserModel[] {
-        return userModelList.map(item => this.mapTo(item));
-    }
-    public static mapTo(userModel: UserDocument): UserModel {
-        return new UserModel({
-            id: userModel.id,
-            firstName: userModel.firstName,
-            lastName: userModel.lastName,
-            username: userModel.username,
-            createdAt: new Date(userModel.createdAt),
-            updatedAt: new Date(userModel.updatedAt)
-        });
-    }
+  public static mapTo(userModel: UserDocument): UserModel {
+    return new UserModel({
+      id: userModel.id,
+      firstName: userModel.firstName,
+      lastName: userModel.lastName,
+      username: userModel.username,
+      password: userModel.password,
+      createdAt: new Date(userModel.createdAt),
+      updatedAt: new Date(userModel.updatedAt),
+    });
+  }
 }
